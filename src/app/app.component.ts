ac1ea83dccb85f59ff16bc16b9e37664051522e4
InaequalitasAeterna <93828337+InaequalitasAeterna@users.noreply.github.com>
2022-11-01 17:02:40 +0200

    lab3 v1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-     
+const pi: number = 3.14159265359;    
 @Component({
     selector: 'my-app',
     template: `<label>Введіть і’мя:</label>
-                 <input [(ngModel)]="name" placeholder="name">
-                 <h1>Вітаємо вас  {{name}}!</h1>`
+                 <input [(ngModel)]="name" placeholder="R">
+                 <h1>Довжина кола  {{2}*{pi}*{R}}</h1>
+                 <h1>площа :  {{pi}*{Math.pow(R)}}</h1>`
 })
 export class AppComponent {
     name= '';
